6b84fa35d8df08249de07c65cc5393ed39eb292f
SzabolcsMarton <maszabolcs@gmail.com>
2023-01-14 15:18:41 +0100

    create webconfig class

---
+++ src/main/java/com/springbootView/springbootview/configurations/WebConfig.java
@@ -0,0 +1,15 @@
+package com.springbootView.springbootview.configurations;
+
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        WebMvcConfigurer.super.addViewControllers(registry);
+        registry.addViewController("/login").setViewName("auth/login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
