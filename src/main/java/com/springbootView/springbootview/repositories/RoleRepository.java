9caef5bf2204053e6fecdc3e79a3a1308406f0ff
SzabolcsMarton <maszabolcs@gmail.com>
2023-01-16 14:55:51 +0100

    created RoleRepository

---
+++ src/main/java/com/springbootView/springbootview/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.springbootView.springbootview.repositories;
+
+import com.springbootView.springbootview.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
