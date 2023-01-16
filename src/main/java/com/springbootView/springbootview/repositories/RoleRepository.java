b69ab0bea7c27ad08cc8da6a34bda220740ba7c3
SzabolcsMarton <maszabolcs@gmail.com>
2023-01-16 16:00:04 +0100

    added findbyrole method

--- src/main/java/com/springbootView/springbootview/repositories/RoleRepository.java
+++ src/main/java/com/springbootView/springbootview/repositories/RoleRepository.java
@@ -4,4 +4,5 @@ import com.springbootView.springbootview.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRole(String role);
 }
